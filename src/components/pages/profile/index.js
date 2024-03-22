409c1fb38c7ddd043a361cc692ac03edacd2dd3b
Gian Braga <gianbraga1802@gmail.com>
2024-03-22 11:47:51 -0300

    arrumando o icon

--- src/components/pages/profile/index.js
+++ src/components/pages/profile/index.js
@@ -5,7 +5,7 @@ import Modal from '../../modal';
 import RenderCards from '../../renderCards';
 
 export default function PageProfile(props) {
-    const [isModalVisible, setModalVisibility] = useState(false); 
+    const [isModalVisible, setModalVisibility] = useState(false);
 
     function modalClickHandler() {
         setModalVisibility(true);
@@ -15,31 +15,31 @@ export default function PageProfile(props) {
         props.getNewUserIconApp(e);
     }
 
-    return(
-       <>
-            <Header 
-                logOutHome={(e) => props.logOutApp(e)} 
-                userIcon={props.userIcon} 
-                pokemonCount={props.pokemonCount} 
+    return (
+        <>
+            <Header
+                logOutHome={(e) => props.logOutApp(e)}
+                userIcon={props.userIcon}
+                pokemonCount={props.pokemonCount}
                 username={props.username}
             />
             <div className='direction'>
-            <button onClick={modalClickHandler}  className='perfil-edit'>Editar perfil</button>
-            <h1>Seus pokemons favoritos:</h1>
-                <RenderCards 
+                <button onClick={modalClickHandler} className='perfil-edit'>Editar perfil</button>
+                <h1>Seus pokemons favoritos:</h1>
+                <RenderCards
                     favArray={props.favArray}
-                    getIdCard={(e) => props.getIdPage(e)} 
+                    getIdCard={(e) => props.getIdPage(e)}
                     justFavCards={true}
                 />
-                {isModalVisible ? (<Modal 
-                                        userIcon={props.userIcon} 
-                                        getNewUserIconProfile={(e) => getNewUserIconProfile(e)} 
-                                        getNewNickProfile={(e) => props.getNewNickApp(e)} 
-                                        username={props.username} 
-                                        close={() => setModalVisibility(false)} 
-                                        getBackgroundColor={(e) => props.getBackgroundColorApp(e)}
-                                    />) : null} 
+                {isModalVisible ? (<Modal
+                    userIcon={props.userIcon}
+                    getNewUserIconProfile={(e) => getNewUserIconProfile(e)}
+                    getNewNickProfile={(e) => props.getNewNickApp(e)}
+                    username={props.username}
+                    close={() => setModalVisibility(false)}
+                    getBackgroundColor={(e) => props.getBackgroundColorApp(e)}
+                />) : null}
             </div>
-       </> 
+        </>
     );
 }
